d46bf5843e60c01b6ca721aaa07105f6129f1f04
r3dDoX <patrick@walther-online.ch>
2016-03-08 16:24:01 -0800

    remove Meteor since we never get to the server part\nif we ever should, make it plain

--- webpong.js
+++ webpong.js
@@ -1,17 +1,11 @@
-if (Meteor.isClient) {
-    Meteor.startup(function () {
-        var canvas = document.getElementById("canvasMain"),
-            game = new Game(canvas);
+(function() {
+  document.addEventListener('DOMContentLoaded', function() {
+    var canvas = document.getElementById("canvasMain"),
+        game = new Game(canvas);
 
-        window.addEventListener('resize', game.resizeCanvas, false);
+    window.addEventListener('resize', game.resizeCanvas, false);
 
-        game.resizeCanvas();
-        game.startGame();
-    });
-}
-
-if (Meteor.isServer) {
-    Meteor.startup(function () {
-
-    });
-}
+    game.resizeCanvas();
+    game.startGame();
+  });
+})();
